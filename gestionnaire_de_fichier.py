97cd7d8b090a4a954f6792427a749921d08ab6cf
Pathlib <gautroncyber@proton.me>
2026-03-11 10:43:15 +0100

    Update gestionnaire_de_fichier.py

--- gestionnaire_de_fichier.py
+++ gestionnaire_de_fichier.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import os
-
+import shutil
 
 def clear_console():
-    if os.name == "nt":  
+    if os.name == "nt":
         os.system("cls")# Windows
-    else:  
+    else:
         os.system("clear")# macOS / Linux
 
 
@@ -24,11 +24,10 @@ def m():
     for nom, icone in dossiers:
         print(f"[{icone}] {nom}")
 
-
 def affiche():
-    a=input("Desktop")
-    b=input("fichier ")
-    donnee = Path.home()/a/b
+    a = input("Desktop")
+    b = input("fichier ")
+    donnee = Path.home() / a / b
     if donnee.exists():
         if donnee.is_dir():
             return donnee
@@ -36,45 +35,54 @@ def affiche():
             return donnee
 
 
-def ouvrir():
-    chemin=Path.home()/"Desktop"/"n.txt"
+def ouvrir(b):
+    chemin=Path.home()/b
     os.startfile(chemin)
+#a metre pour linux et mac
 
-
-def supprimer():
-    b=input("vouler vous vraiment supprimer ?")
-    if b == "oui":
-        chemin=Path.home()/"Desktop"/"n.txt"
-        Path(chemin).unlink()
-    else :
-        return
+def supprimer(b):
+    c=input("vouler vous vraiment supprimer ?")
+    if c == "oui":
+        chemin=Path.home()/b
+        if chemin.is_file():
+            Path(chemin).unlink()
+        else :
+            shutil.rmtree(Path(chemin))
 
 def creer():
-    chemin=Path.home()/"Desktop"/"n.txt"
-    with open(chemin,"w") as fichier:
-        print(fichier)
+    a=input("fichier ou dossier")
+    chemin = Path.home() / "Desktop" / "nh.txt"
+    if a == "fichier":
+        chemin.touch()
+    else:
+        chemin.mkdir()
 
 def renomer():
-    chemin=Path.home()/"Desktop"/"n.txt"
+    chemin=Path.home()/"Desktop"/"nh.txt"
+    nchemin=Path.home()/"Desktop"/"nkl.txt"
+    Path(chemin).rename(nchemin)
 
 def copier():
     chemin=Path.home()/"Desktop"/"n.txt"
+    nchemin=Path.home()/"Documents"/"n.txt"
+    shutil.copy2(Path(chemin), Path(nchemin))
+
 
 while True:
     clear_console()
     print("bienvenue")
     m()
-    a=input("affiche ouvrir supprimer creer")
+    b=affiche()
+    a=input("affiche ouvrir supprimer creer renomer copier")
     if a == "affiche":
         affiche()
-
     elif a == "ouvrir":
-        ouvrir()
-
+        ouvrir(b)
     elif a == "supprimer":
-        supprimer()
+        supprimer(b)
     elif a == "creer":
         creer()
     elif a == "renomer":
         renomer()
-
+    elif a == "copier":
+        copier()
